1f884080d72cbf113db2cc8a5729685c82daacf8
Victor Wongsatjachock <victorwongsatjachock@gmail.com>
2023-10-31 14:27:30 -0700

    fix connection

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL = 
-    'mongodb+srv://admin:ch9WegCVC0nft6aj@322.q77ybms.mongodb.net/322?retryWrites=true&w=majority'
+    'mongodb+srv://admin:ch9WegCVC0nft6aj@322.q77ybms.mongodb.net/resume322?retryWrites=true&w=majority'
