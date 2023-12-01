c7085291eb065cc16b9f869b3c477bf9fe69a4c8
Dennis <78121532+dennissantoso@users.noreply.github.com>
2023-12-01 15:34:06 -0800

    adding margin center

--- client/src/Templates.jsx
+++ client/src/Templates.jsx
@@ -21,7 +21,7 @@ const Templates = () => {
                 <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">Templates</h1>
                 <p className="pt-4 w-60 sm:w-72 md:w-80 leading-6">  Feel free to use these templates to start your new Resume </p>
                 <br/> <br/> <br/>
-                <div className="flex gap-x-9 items-center justify-center">
+                <div className="flex gap-x-9 items-center margin-center justify-center">
                   <img src={images.RESUME1} alt="resume1" height={400} width={400}></img> <br/>
                   <img src={images.RESUME2} alt="resume2" height={400} width={400}></img> <br/>
                   <img src={images.RESUME3} alt="resume3" height={400} width={400}></img> <br/>
