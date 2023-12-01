3e4dce96c558d367fe49a65009a0fbc9397b9e0b
Dennis <13dennissantoso@gmail.com>
2023-11-30 19:49:32 -0800

    adding templates

--- client/src/Templates.jsx
+++ client/src/Templates.jsx
@@ -4,6 +4,7 @@ import Navbar from './NavBar'
 import Header from './Header'
 import EditorContext from "./editorContext"
 import Footer from './Footer'
+import { images } from "./constants";
 
 
 
@@ -16,11 +17,15 @@ const Templates = () => {
   return (
     <EditorContext.Provider value={contextValue}>
     <Header/>
-
-    
         <div className="bg-gray-300 py-20 px-12" align="center">
-                <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold"> Templates </h1>
+                <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">Templates</h1>
                 <p className="pt-4 w-60 sm:w-72 md:w-80 leading-6">  Feel free to use these templates to start your new Resume </p>
+                <br/> <br/> <br/>
+                <div className="flex gap-x-9 items-center justify-center">
+                  <img src={images.RESUME1} alt="resume1" height={400} width={400}></img> <br/>
+                  <img src={images.RESUME2} alt="resume2" height={400} width={400}></img> <br/>
+                  <img src={images.RESUME3} alt="resume3" height={400} width={400}></img> <br/>
+                </div>
         </div>
     <Footer/>
     </EditorContext.Provider>
--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -1,7 +1,10 @@
 import DVLlogo from "../assets/DVLlogo.svg";
+import RESUME1 from "../assets/RESUME1.svg";
+import RESUME2 from "../assets/RESUME2.svg";
+import RESUME3 from "../assets/RESUME3.svg";
 
 const images = {
-    DVLlogo
+    DVLlogo, RESUME1, RESUME2, RESUME3
 };
 
 export default images;
