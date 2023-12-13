1459379d8727526bfef8d278e05a672411d0e2fd
Victor Wongsatjachock <victorwongsatjachock@gmail.com>
2023-12-12 21:25:26 -0800

    add backend connection to frontend stuff

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}></Provider>
     <BrowserRouter>
       <App/>
     </BrowserRouter>
