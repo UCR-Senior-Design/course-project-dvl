ba3dc2731b6a74e37070579b05a7da4ed13389bc
Lester Lien <lesterlien11@gmail.com>
2023-12-12 21:57:09 -0800

    routing contactus

--- client/src/ContactUs.jsx
+++ client/src/ContactUs.jsx
@@ -11,9 +11,8 @@ const ContactUs = () => {
 
   return (
     <div className="bg-gray-300">
-        <div>
+            <div>
             <HomeNav/>
-        </div>
             
             <section class="bg-white dark:bg-gray-900">
                 <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
@@ -32,11 +31,10 @@ const ContactUs = () => {
                             <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
                             <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
                         </div>
-                        <button type="submit" class="py-3 px-5 text-sm border font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+                        <button class="py-3 px-5 text-sm border font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={() => navigate("/HomePage")}>Send message</button>
                     </form>
                 </div>
-                </section>            
-        <div>
+                </section>  
             <Footer/>
         </div>
     </div>
