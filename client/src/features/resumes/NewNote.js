7ed6e733394cea9c563d2ca2a8ee789acb3dd57c
cwongsatjachock <victorwongsatjachock@gmail.com>
2024-01-16 18:38:21 -0800

    continued tutorial, same problem as before

--- client/src/features/resumes/NewNote.js
+++ client/src/features/resumes/NewNote.js
@@ -5,6 +5,8 @@ import NewResumeForm from './NewResumeForm'
 const NewResume = () => {
     const users = useSelector(selectAllUsers)
 
+    if (!users?.length) return <p>Not Currently Available</p>
+
     const content = users ? <NewResumeForm users={users} /> : <p>Loading...</p>
 
     return content
