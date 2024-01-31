251df1948a2d32bc26f24a512af72cb33640dcff
Victor Wongsatjachock <victorwongsatjachock@gmail.com>
2024-01-31 14:57:03 -0800

    nvm

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 // import { TEInput } from "tw-elements-react";
 import { useNavigate } from "react-router-dom";
 // import MainLayout from './MainLayout'
 import HomeNav from './HomeNav';
 import Footer from './Footer';
+import { GoogleLogin } from '@react-oauth/google';
+
+import { signin } from './actions/auth.js';
+import { AUTH } from './constants/actionTypes';
+
+const initialState = { name: '', email: '', password: ''};
 
 const Login = () => {
-    
-    const [checked, setChecked] = React.useState(false);
+    const [form, setForm] = useState(initialState);
+    const dispatch = useDispatch();
+    // const [checked, setChecked] = React.useState(false);
 
-    const handleChange = () => {
-        setChecked(!checked);
-    }
+    // const handleChange = () => {
+    //     setChecked(!checked);
+    // }
 
     const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(signin(form, navigate('/HomePage')));
+      };
+
+    const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
   return (
 
@@ -25,26 +40,27 @@ const Login = () => {
                 <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold"> Welcome Back! </h1>
                 <p className="pt-4 w-72 sm:w-72 md:w-96 leading-relaxed">  At DVL we are continually striving to improve accessibility of our products
                 and services. If you have any questions or suggestions regarding our services, we welcome your feedback. </p>
+                <form onSubmit={handleSubmit}>
+                    
                 <div>
-                <div>
-                    <label for="name" class="block mt-4 mb-2 text-lg font-medium text-gray-900 dark:text-gray-600">Full Name</label>
-                    <input name="name" id="Name" handleChange={handleChange} class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="" required/>
+                    <label for="email" class="block mt-4 mb-2 text-lg font-medium text-gray-900 dark:text-gray-600">Your email</label>
+                    <input name="email" type="email" id="email" onChange={handleChange} class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@gmail.com" required/>
                 </div>
                 <div>
                     <label for="password" class="block mt-4 mb-2 text-lg font-medium text-gray-900 dark:text-gray-600">Password</label>
-                    <input type="password" id="Password" handleChange={handleChange} class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="" required/>
+                    <input name="password" type="password" id="password" onChange={handleChange} class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="" required/>
                 </div>
-                     <button type="submit" className="mt-5 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white">Login</button> 
-                </div>
+                <button type="submit" className="mt-5 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white">Login</button>
+                </form>
 
-                <div className="mt-3 mb-3">
+                {/* <div className="mt-3 mb-3">
                     <Checkbox
                         // style={{transform: "scale(5)",}}
                         label = " Remember my email and password"
                         value ={checked}
                         onChange={handleChange}
                         />
-                </div>
+                </div> */}
                 
                 <div>
                 Need help logging in? {" "}
--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -12,7 +12,7 @@ import { AUTH } from './constants/actionTypes';
 
 const initialState = { name: '', email: '', password: ''};
 
-const Login = () => {
+const Signup = () => {
     const [form, setForm] = useState(initialState);
     const dispatch = useDispatch();
 
@@ -62,17 +62,17 @@ const Login = () => {
                 <p className="pt-4 w-72 sm:w-72 md:w-96 leading-relaxed">  Resume 322 is commiteed to help new people in the industry to create a better environment for making Resume. </p>
                 <form onSubmit={handleSubmit}>
                     <div>
-                            <label for="name" class="block mt-4 mb-2 text-lg font-medium text-gray-900 dark:text-gray-600">Full Name</label>
-                            <input name="name" id="name" onChange={handleChange} class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="" required/>
-                        </div>
+                        <label for="name" class="block mt-4 mb-2 text-lg font-medium text-gray-900 dark:text-gray-600">Full Name</label>
+                        <input name="name" id="name" onChange={handleChange} class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="" required/>
+                    </div>
                     <div>
-                            <label for="email" class="block mt-4 mb-2 text-lg font-medium text-gray-900 dark:text-gray-600">Your email</label>
-                            <input name="email" type="email" id="email" onChange={handleChange} class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@gmail.com" required/>
-                        </div>
-                        <div>
-                            <label for="password" class="block mt-4 mb-2 text-lg font-medium text-gray-900 dark:text-gray-600">Password</label>
-                            <input name="password" type="password" id="password" onChange={handleChange} class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="" required/>
-                        </div>
+                        <label for="email" class="block mt-4 mb-2 text-lg font-medium text-gray-900 dark:text-gray-600">Your email</label>
+                        <input name="email" type="email" id="email" onChange={handleChange} class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@gmail.com" required/>
+                    </div>
+                    <div>
+                        <label for="password" class="block mt-4 mb-2 text-lg font-medium text-gray-900 dark:text-gray-600">Password</label>
+                        <input name="password" type="password" id="password" onChange={handleChange} class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="" required/>
+                    </div>
                         <button type="submit" className= "mt-5 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white">Let's Start</button>
                             
                         {/* <GoogleLogin
@@ -135,4 +135,4 @@ const Checkbox2 = ({label2, value2, onChange2}) => {
         </label>
     )
 }
-export default Login;
+export default Signup;
