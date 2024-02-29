f091a2f4b28a50e5145dd023f4efb138dec69a7b
LesterLien <lesterlien11@gmail.com>
2024-02-29 02:22:52 -0800

    PDF format almost done

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -1,15 +1,12 @@
 import React, { useContext, useRef, useState } from 'react';
-import MainLayout from './MainLayout'
-import { Layout } from './layout/Layout'
-import EditorContext from "./editorContext"
-import Navbar from './NavBar'
-import Header from './Header'
-import Footer from './Footer'
+import MainLayout from './MainLayout';
+import { Layout } from './layout/Layout';
+import EditorContext from './editorContext';
+import Header from './Header';
+import Footer from './Footer';
 import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
-import editorContext from './editorContext';
-import { jsPDF } from "jspdf";
-
+import { jsPDF } from 'jspdf';
 
 const Container = styled.div`
   width: 50%;
@@ -25,14 +22,11 @@ const PreviewArea = styled.div`
   outline: none;
 `;
 
-
 const HomePage = () => {
-  const  [markdownText, setMarkdownText] = useState("");
+  const [markdownText, setMarkdownText] = useState("");
   const contextValue = {
     markdownText,
     setMarkdownText
-
-  
   };
   const pdfRef = useRef(null);
 
@@ -46,35 +40,33 @@ const HomePage = () => {
       margin: [20, 20, 20, 20],
       autoPaging: 'text',
     };
-
-    jspdf.html(pdfRef.current.innerHTML, data);
+    jspdf.html(pdfRef.current, data);
   };
+
   return (
     <EditorContext.Provider value={contextValue}>
-    <Header/>
-    <div>
+      <Header />
+      <div>
         <div className="bg-gray-300 py-20 px-12">
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold"> Easy Ways to Build a Beautiful Resume </h1>
-          <p className="pt-4 w-60 sm:w-72 md:w-96 leading-6">  Ditch your google doc with a better resume crafting experience </p>
-          <div class="flex justify-center items-center">
-          <button className= "flex justify-center border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white"onClick={handleGeneratePDF}>Generate PDF</button>
+          <p className="pt-4 w-60 sm:w-72 md:w-96 leading-6">  Ditch your Google Doc with a better resume crafting experience </p>
+          <div className="flex justify-center items-center">
+            <button className="flex justify-center border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white" onClick={handleGeneratePDF}>Generate PDF</button>
           </div>
         </div>
         <div className='w-full h-full flex pb-1'>
           <Layout />
           <Container>
-          <h1 className="font-bold text-xl text-center border-b-4 border-gray-500/40">Preview</h1>
-          <PreviewArea className='preview overflow-y-scroll border-b-4' ref={pdfRef}>
-        <ReactMarkdown children={markdownText} className="markdown" />
-      </PreviewArea>
-        </Container>
+            <h1 className="font-bold text-xl text-center border-b-4 border-gray-500/40">Preview</h1>
+            <PreviewArea className='preview overflow-y-scroll border-b-4' ref={pdfRef}>
+              <ReactMarkdown children={markdownText} className="markdown" />
+            </PreviewArea>
+          </Container>
         </div>
-    </div>
-    <Footer/>
+      </div>
+      <Footer />
     </EditorContext.Provider>
   );
 };
 
 export default HomePage;
-
-
