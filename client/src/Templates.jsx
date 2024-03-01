60ab749392a47f5f511cf03076a4afa5dee82595
LesterLien <lesterlien11@gmail.com>
2024-02-29 21:51:49 -0800

    Clickable template

--- client/src/Templates.jsx
+++ client/src/Templates.jsx
@@ -5,7 +5,7 @@ import Header from './Header'
 import EditorContext from "./editorContext"
 import Footer from './Footer'
 import { images } from "./constants";
-
+import { useNavigate, useLocation } from 'react-router-dom';
 
 
 const Templates = () => {
@@ -14,6 +14,7 @@ const Templates = () => {
     markdownText,
     setMarkdownText
   };
+  const navigate = useNavigate();
   return (
     <EditorContext.Provider value={contextValue}>
     <Header/>
@@ -22,9 +23,15 @@ const Templates = () => {
                 <p className="pt-4 w-60 sm:w-72 md:w-80 leading-6">  Feel free to use these templates to start your new Resume </p>
                 <br/> <br/> <br/>
                 <div className="flex gap-x-9 items-center margin-center justify-center">
-                  <img src={images.RESUME1} alt="resume1" height={400} width={400}></img> <br/>
-                  <img src={images.RESUME2} alt="resume2" height={400} width={400}></img> <br/>
-                  <img src={images.RESUME3} alt="resume3" height={400} width={400}></img> <br/>
+                  <div>
+                    <img src={images.RESUME1} alt="resume1" height={400} width={400} onClick={() => navigate("/HomePage")} ></img> <br/>
+                  </div>
+                  <div>
+                    <img src={images.RESUME2} alt="resume2" height={400} width={400} onClick={() => navigate("/HomePage")} ></img> <br/>
+                  </div>
+                  <div>
+                    <img src={images.RESUME3} alt="resume3" height={400} width={400} onClick={() => navigate("/HomePage")} ></img> <br/>
+                  </div>
                 </div>
         </div>
     <Footer/>
